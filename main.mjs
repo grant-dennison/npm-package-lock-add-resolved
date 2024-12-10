66906a5b091a8caae00e0c9b2ad5c4f34ca90620
Grant Dennison <grant.dennison@gmail.com>
2024-12-10 16:49:19 -0500

    Make tool more backwards compatible and add test

--- main.mjs
+++ main.mjs
@@ -1,17 +1,28 @@
 #!/usr/bin/env node
 
-import assert from "node:assert"
 import { spawnSync } from "node:child_process"
 import { readFile, writeFile } from "node:fs/promises"
+import { get } from "node:https"
 
-const oldContents = await readFile("package-lock.json", "utf-8")
-const packageLock = JSON.parse(oldContents)
+function fetchJson(url) {
+  return new Promise((resolve, reject) => {
+    get(url, (res) => {
+      let data = ""
+      res.on("data", (chunk) => {
+        data += chunk
+      })
+      res.on("end", () => {
+        resolve(JSON.parse(data))
+      })
+
+      res.on("error", reject)
+    }).on("error", reject)
+  })
+}
 
 async function fillResolved(name, p) {
   const metadataUrl = `https://registry.npmjs.com/${name}/${p.version}`
-  const response = await fetch(metadataUrl)
-  assert(response.ok)
-  const metadata = await response.json()
+  const metadata = await fetchJson(metadataUrl)
 
   p.resolved = metadata.dist.tarball
   p.integrity = metadata.dist.integrity
@@ -26,15 +37,18 @@ async function fillAllResolved(list) {
     if (p.resolved && p.integrity) {
       continue
     }
-  
+
     const packageName = packagePath.replace(/^.*node_modules\/(?=.+?$)/, "")
     await fillResolved(packageName, p)
 
     if (p.dependencies) {
-      fillAllResolved(p.dependencies)
+      await fillAllResolved(p.dependencies)
     }
   }
 }
+
+const oldContents = await readFile("package-lock.json", "utf-8")
+const packageLock = JSON.parse(oldContents)
 
 await fillAllResolved(packageLock.packages ?? [])
 await fillAllResolved(packageLock.dependencies ?? [])
@@ -43,7 +57,7 @@ await writeFile("package-lock.json", JSON.stringify(packageLock, null, 2))
 
 try {
   // npm install will validate and reformat
-  spawnSync('npm', ['install'], { stdio: 'inherit', shell: true })
+  spawnSync("npm", ["install"], { stdio: "inherit", shell: true })
 } catch (e) {
   console.error("Rolling back package-lock.json changes")
   await writeFile("package-lock.json", oldContents)
