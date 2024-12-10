3f144fadcfc9e0841e0e29a0faf9c222d4110bbb
Grant Dennison <grant.dennison@gmail.com>
2024-12-10 15:45:26 -0500

    Implement tool

--- README.md
+++ README.md
@@ -2,8 +2,25 @@
 
 This is a simple npm tool to fill missing `resolved` and `integrity` fields
 in npm `package-lock.json` files.
+
+It would seem that these fields can be omitted by npm
+when it is able to use locally cached versions of dependencies
+but does not have information available regarding origin.
+
 Some discussion surrounding the absence of these fields can be found at
 https://github.com/npm/cli/issues/4460
 
 Implementation for this tool was derived from
 https://github.com/jeslie0/npm-lockfile-fix
+
+## Usage
+
+```
+npx npm-package-lock-add-resolved
+```
+
+If you want to use a Docker container instead:
+
+```
+docker run --rm -it -v $(pwd):/w --workdir /w node npx npm-package-lock-add-resolved
+```
---
+++ main.mjs
@@ -0,0 +1,49 @@
+import assert from "node:assert"
+import { spawnSync } from "node:child_process"
+import { readFile, writeFile } from "node:fs/promises"
+
+const oldContents = await readFile("package-lock.json", "utf-8")
+const packageLock = JSON.parse(oldContents)
+
+async function fillResolved(name, p) {
+  const metadataUrl = `https://registry.npmjs.com/${name}/${p.version}`
+  const response = await fetch(metadataUrl)
+  assert(response.ok)
+  const metadata = await response.json()
+
+  p.resolved = metadata.dist.tarball
+  p.integrity = metadata.dist.integrity
+}
+
+async function fillAllResolved(list) {
+  for (const packagePath in list) {
+    if (packagePath === "") {
+      continue
+    }
+    const p = list[packagePath]
+    if (p.resolved && p.integrity) {
+      continue
+    }
+  
+    const packageName = packagePath.replace(/^.*node_modules\/(?=.+?$)/, "")
+    await fillResolved(packageName, p)
+
+    if (p.dependencies) {
+      fillAllResolved(p.dependencies)
+    }
+  }
+}
+
+await fillAllResolved(packageLock.packages ?? [])
+await fillAllResolved(packageLock.dependencies ?? [])
+
+await writeFile("package-lock.json", JSON.stringify(packageLock, null, 2))
+
+try {
+  // npm install will validate and reformat
+  spawnSync('npm', ['install'], { stdio: 'inherit', shell: true })
+} catch (e) {
+  console.error("Rolling back package-lock.json changes")
+  await writeFile("package-lock.json", oldContents)
+  throw e
+}
